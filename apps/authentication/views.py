39416779005fae2359b1ffa5660e4a0a175963d7
lscavalcante <lucasantoscv@gmail.com>
2021-05-28 01:46:32 -0300

    ..

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -43,6 +43,8 @@ class RegisterView(generics.GenericAPIView):
 
         Util.send_email(data)
 
+        print('new pull request')
+
         return Response(user_data, status=status.HTTP_201_CREATED)
 
 
